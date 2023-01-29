46017ad9590b9a8a6d22692d9909d7c1e4608d59
Alex Tennyson-Davies <alex.tennysondavies@gmail.com>
2023-01-29 00:57:33 +0000

    Added docs. Started adding decoder for bytesteam

---
+++ BitTorrentClient/Application/BEncoding.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace BitTorrentClient.Application;
+
+public class BEncoding
+{
+
+    private const byte DictionaryStart = 100; // d
+    private static byte DictionaryEnd = System.Text.Encoding.UTF8.GetBytes("e")[0]; // 101
+    private const byte ListStart = 108; // l
+    private static byte ListEnd = System.Text.Encoding.UTF8.GetBytes("e")[0]; // 101
+    private const byte NumberStart = 105; // i
+    private static byte NumberEnd = System.Text.Encoding.UTF8.GetBytes("e")[0]; // 101
+    private static byte ByteArrayDivider = System.Text.Encoding.UTF8.GetBytes(":")[0]; //  58
+
+    public static object Decode(byte[] bytes)
+    {
+        IEnumerator<byte> enumerator = (IEnumerator<byte>)bytes.GetEnumerator();
+        enumerator.MoveNext();
+        return DecodeNextObject(enumerator);
+    }
+
+    private static object DecodeNextObject(IEnumerator<byte> enumerator) => enumerator.Current switch
+    {
+        DictionaryStart => DecodeDictionary(enumerator),
+        ListStart => DecodeList(enumerator),
+        NumberStart => DecodeNumber(enumerator),
+        _ => DecodeByteArray(enumerator)
+    };
+
+    private static object DecodeByteArray(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException();
+    }
+    private static object DecodeNumber(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException();
+    }
+    private static object DecodeList(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException();
+    }
+    private static object DecodeDictionary(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Strings 9:somewords
+    //Integers i4e
+    //Lists l4spam:3lole
+    //Dictsd d8:greeting5:hello4:name4:jakee
+}
+
---
+++ docs/BEncoding.md
@@ -0,0 +1,2 @@
+# BEncoding
+[Bit torrent spec](http://www.bittorrent.org/beps/bep_0003.html)
